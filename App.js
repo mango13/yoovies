48dd921012a7939cae6f1dd7ab7b97671c32d235
emyoo <emyoo@nkia.co.kr>
2020-12-17 15:19:06 +0900

    2.3 Stack Navigation

--- App.js
+++ App.js
@@ -1,34 +1,44 @@
 import React, { useState } from "react";
-import { Text, Image } from "react-native";
+import { Image } from "react-native";
 import AppLoading from "expo-app-loading";
 import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import {NavigationContainer} from "@react-navigation/native";
+import Stack from "./navigaition/Stack";
+
 
 const cacheImages = (images) =>
-    images.map((image) => {
-      if (typeof image === "string") {
-        return Image.prefetch(image);
-      } else {
-        return Asset.fromModule(image).downloadAsync();
-      }
-    });
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+
+const cacheFonts = (fonts) => fonts.map((font) => [Font.loadAsync(font), Font.loadAsync(font)]);
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
-  const loadAssets = async () => {
+  const loadAssets = () => {
     const images = cacheImages([
       "https://images.unsplash.com/photo-1482517716521-3120e5340ed3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
       require("./assets/splash.png"),
     ]);
-    console.log(images);
+    const fonts = cacheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
   };
   const onFinish = () => setIsReady(true);
   return isReady ? (
-      <Text>Ready!</Text>
+    <NavigationContainer>
+      <Stack />
+    </NavigationContainer>
   ) : (
-      <AppLoading
-          startAsync={loadAssets}
-          onFinish={onFinish}
-          onError={console.error}
-      />
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish}
+      onError={console.error}
+    />
   );
 }
