a2975e4c2676761e3da263bee0cd81353761f27c
mango <disnl9755@gmail.com>
2021-01-03 19:04:21 +0900

    #8.6 Changing Card on Top

--- screens/Favs/FavsPresenter.js
+++ screens/Favs/FavsPresenter.js
@@ -27,19 +27,37 @@ const styles = {
 
 export default ({ results }) => {
   const [topIndex, setTopIndex] = useState(0);
+  const nextCard = () => setTopIndex(currentValue => currentValue + 1);
   const position = new Animated.ValueXY();
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (evt, { dx, dy }) => {
       position.setValue({ x: dx, y: dy });
     },
-    onPanResponderRelease: () => {
-      Animated.spring(position, {
-        toValue: {
-          x: 0,
-          y: 0,
-        },
-      }).start();
+    onPanResponderRelease: (evt, { dx, dy }) => {
+      if (dx >= 250) {
+        Animated.spring(position, {
+          toValue: {
+            x: WIDTH + 100,
+            y: dy,
+          },
+        }).start(nextCard);
+      } else if (dx <= -250) {
+        Animated.spring(position, {
+          toValue: {
+            x: -WIDTH - 100,
+            y: dy,
+
+          },
+        }).start(nextCard);
+      } else {
+        Animated.spring(position, {
+          toValue: {
+            x: 0,
+            y: 0,
+          },
+        }).start(nextCard);
+      }
     },
   });
 
@@ -64,6 +82,9 @@ export default ({ results }) => {
   return (
     <Container>
       {results.map((result, index) => {
+        if(index < topIndex) {
+          return null;
+        }
         if (index === topIndex) {
           return (
             <Animated.View
@@ -81,39 +102,36 @@ export default ({ results }) => {
               <Poster source={{ uri: apiImage(result.poster_path) }} />
             </Animated.View>
           );
-        } else if(index === topIndex + 1) {
+        } else if (index === topIndex + 1) {
           return (
-              <Animated.View
-                  style={{
-                    ...styles,
-                    zIndex: -index,
-                    opacity: secondCardOpacity,
-                    transform: [
-                      { scale: secondCardScale },
-                    ],
-                  }}
-                  key={result.id}
-                  {...panResponder.panHandlers}
-              >
-                <Poster source={{ uri: apiImage(result.poster_path) }} />
-              </Animated.View>
+            <Animated.View
+              style={{
+                ...styles,
+                zIndex: -index,
+                opacity: secondCardOpacity,
+                transform: [{ scale: secondCardScale }],
+              }}
+              key={result.id}
+              {...panResponder.panHandlers}
+            >
+              <Poster source={{ uri: apiImage(result.poster_path) }} />
+            </Animated.View>
           );
         } else {
           return (
-              <Animated.View
-                  style={{
-                    ...styles,
-                    zIndex: -index,
-                    opacity: 0,
-                  }}
-                  key={result.id}
-                  {...panResponder.panHandlers}
-              >
-                <Poster source={{ uri: apiImage(result.poster_path) }} />
-              </Animated.View>
+            <Animated.View
+              style={{
+                ...styles,
+                zIndex: -index,
+                opacity: 0,
+              }}
+              key={result.id}
+              {...panResponder.panHandlers}
+            >
+              <Poster source={{ uri: apiImage(result.poster_path) }} />
+            </Animated.View>
           );
         }
-
       })}
     </Container>
   );
